842ab459d787fd86492d6a4c3e3257d57df0cd6a
m3rcyS0ber <the-manager@mail.ru>
2023-08-06 22:15:13 +0400

    v1.0

--- js/script.js
+++ js/script.js
@@ -47,22 +47,9 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
-    let touchstartX = 0
-    let touchendX = 0
-
-    function checkDirection() {
-        if (touchendX < touchstartX) alert('swiped left!')
-        if (touchendX > touchstartX) alert('swiped right!')
-    }
-
-    document.addEventListener('touchstart', e => {
-        touchstartX = e.changedTouches[0].screenX
-    })
-
-    document.addEventListener('touchend', e => {
-        touchendX = e.changedTouches[0].screenX
-        checkDirection()
-    })
+    $("body").on("swipe", function(event) {
+        alert("It's a swipe!");
+    });
     function slideTo(to) {
 
         let prevIndicator = document.querySelector('.history .indicators .indicator.active');
